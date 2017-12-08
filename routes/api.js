138e7297c7e5dbce99e95a694ded966dbd0bdda0
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-08 12:12:47 -0800

    Builds file directories for React front-end, and links bootstrap cdn.

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router;
+const router = express.Router();
 const orderCtrl = require('../controllers/ordersController');
 
 router.get('/products', orderCtrl.getProducts);
