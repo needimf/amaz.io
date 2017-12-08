0c7a8572931f99eba0934f3abe189d358cc5f58c
Nedim Filipovic <nedim.filipovic@gmail.com>
2017-12-08 14:28:35 -0800

    Styles navbar.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const NavBar = (props) => {
   return (
     <div>
-      <nav>
-        Amaz.io
+      <nav className="navbar sticky-top navbar-dark bg-dark">
+        <Link className="navbar-brand ml-4" to="/">Amaz.io</Link>
+        <ul className="navbar-nav navbar-right">
+            <Link to="/checkout" ><li className="text-white mr-4"><i className="material-icons" style={{"font-size": "35px"}}>shopping_cart</i></li></Link>
+        </ul>
       </nav>
     </div>
   );
